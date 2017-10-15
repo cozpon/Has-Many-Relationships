d58822d7de63f4d609148e4215e49f80394742bb
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-14 14:56:46 -1000

    finished

--- joins.sql
+++ joins.sql
@@ -79,13 +79,13 @@
 -- AND    p.content LIKE '%dolorum%';
 
 -- 13
-SELECT u.first_name AS "post_author_first_name", u.last_name AS "post_author_last_name",
-       p.title AS "post_title", u.username AS "comment_author_username", c.body AS "comment_body"
-FROM   comments c
-JOIN   posts p ON p.id = c.posts_id
-JOIN   users u ON u.id = c.users_id
-WHERE  p.content LIKE '%nemo%'
-AND    (c.body LIKE '%SSL%' OR c.body LIKE '%firewall%');
+-- SELECT u.first_name AS "post_author_first_name", u.last_name AS "post_author_last_name",
+--        p.title AS "post_title", u.username AS "comment_author_username", c.body AS "comment_body"
+-- FROM   comments c
+-- JOIN   posts p ON p.id = c.posts_id
+-- JOIN   users u ON u.id = c.users_id
+-- WHERE  p.content LIKE '%nemo%'
+-- AND    (c.body LIKE '%SSL%' OR c.body LIKE '%firewall%');
 
 
 -- Additional Queries
